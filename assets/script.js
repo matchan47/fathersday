ea6b7257282cac27a1687882f618496e7b82d2a1
matsuda <matchan3586@gmail.com>
2023-06-05 21:57:41 +0900

    [add] 全ファイル追加

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+// ----- ハンバーガーメニュー -----
+$(".toggle_btn").on("click",function() {
+    var pos;
+    if ($("header").hasClass("open")) {  // トグルボタンを押下した時、
+        $("header").removeClass("open"); // × の時は、open クラス削除
+        $('body').removeClass('fixed').css({'top': 0});
+        window.scrollTo(0, pos); // fixed が解除されズレが生じるので、スクロール位置を元の位置へ戻す
+    } else {
+        $("header").addClass("open"); // 三 の時は、open クラス追加
+        pos = $(window).scrollTop();
+        $('body').addClass('fixed').css({'top': -pos});
+    }
+})
+
+// ----- リンクを押した時にメニューを閉じる -----
+$(".right_list a").on("click",function() {
+    $("header").removeClass("open");
+});
